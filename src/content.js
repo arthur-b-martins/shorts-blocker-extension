74ca678c633279cadc108a33e671c203b9410f12
arthur-b-martins <arthurborgesmartins4@gmail.com>
2026-01-19 12:05:04 -0300

    shorts cointaner after a search removed

--- src/content.js
+++ src/content.js
@@ -5,9 +5,19 @@ function redirectShorts() {
     }
 }
 
-function removeShorts(){
-    const shortsShelves = document.querySelectorAll('ytd-rich-shelf-renderer[is-shorts], ytd-reel-shelf-renderer');
-    shortsShelves.forEach(el => el.remove());
+function removeShorts() {
+    // Lista de seletores atualizada com o novo container da busca
+    const selectors = [
+        'ytd-rich-shelf-renderer[is-shorts]', 
+        'ytd-reel-shelf-renderer',
+        'grid-shelf-view-model', 
+        'ytd-shelf-renderer:has(span#title-text:contains("Shorts"))'
+    ];
+
+    selectors.forEach(selector => {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(el => el.remove());
+    });
 }
 
 redirectShorts();
